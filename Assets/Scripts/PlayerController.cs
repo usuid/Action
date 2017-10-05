616f07e9f7bbbd9046977937ac32e02c94d77ba6
usuid <h3036206@H303N0022.local>
2017-10-05 14:02:11 +0900

    Ladder is completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,15 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour {
 
 	//public
-	public float MaxHP = 100.0f;
-	public float PlayerHP = 100.0f;
+	public float MaxHP = 10.0f;
+	public float PlayerHP = 10.0f;
 	public Slider PlayerHPBar;
 	public Text PlayerHPtext;
 	public float Speed = 1.0f;
 	public bool CanMove = true;
 	public float JumpSpeed = 10f;
-	public GameObject Bullet;
+	public int BulletID;
+	public GameObject[] Bullet;
 	//private
 	private bool canjump = true;
 	private float xspeed;
@@ -22,7 +23,8 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		PlayerHPtext.text = PlayerHP.ToString() + "/"+ MaxHP.ToString();
+		PlayerHPBar.value = PlayerHP / MaxHP;
 	}
 	
 	// Update is called once per frame
@@ -36,15 +38,17 @@ public class PlayerController : MonoBehaviour {
 				isForwardRight = false;
 			}
 
-			if (!isLadderStay) {
-				if (Input.GetKeyDown (KeyCode.UpArrow) && canjump) {
-					this.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * JumpSpeed);
-					canjump = false;
-				}
-				if (Input.GetKeyUp (KeyCode.UpArrow) && this.GetComponent<Rigidbody2D> ().velocity.y >= 0f) {
-					this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, 0f);
-				}
-			} else {
+			if (Input.GetKeyDown (KeyCode.Z) && canjump) {
+				this.GetComponent<Rigidbody2D> ().AddForce (Vector2.up * JumpSpeed);
+				isLadderStay = false;
+				this.GetComponent<Rigidbody2D> ().gravityScale = 1f;
+				canjump = false;
+			}
+			if (Input.GetKeyUp (KeyCode.Z) && this.GetComponent<Rigidbody2D> ().velocity.y >= 0f) {
+				this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, 0f);
+			}
+
+			if (isLadderStay) {
 				if (Input.GetKey (KeyCode.UpArrow)) {
 					this.transform.Translate (new Vector3 (0f, 0.1f, 0f));
 				}
@@ -53,16 +57,18 @@ public class PlayerController : MonoBehaviour {
 				}
 				this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, 0f);
 			}
-			if (Input.GetKeyDown (KeyCode.Z)) {
-				FireNormalBullet ();
+
+			if (Input.GetKeyDown (KeyCode.X)) {
+				FireBullet ();
 			}
 		}
 	}
 
 	void OnCollisionEnter2D (Collision2D Other) {
 		if (Other.gameObject.CompareTag ("Ground")) {
-			canjump = true; 
 			isLadderStay = false;
+			canjump = true;
+			this.GetComponent<Rigidbody2D> ().gravityScale = 1f;
 		}
 		if (Other.gameObject.CompareTag ("Enemy")) {
 			PlayerHP -= 1f;
@@ -73,16 +79,26 @@ public class PlayerController : MonoBehaviour {
 
 	void OnTriggerStay2D (Collider2D coll){
 		if (coll.gameObject.CompareTag ("Ladder")) {
-			isLadderStay = true;
-			canjump = true;
+			if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.DownArrow)) {
+				isLadderStay = true;
+				canjump = true;
+				this.GetComponent<Rigidbody2D> ().gravityScale = 0f;
+			}
 		}
 	}
 
-	void FireNormalBullet () {
+	void OnTriggerExit2D (Collider2D coll){
+		if (coll.gameObject.CompareTag ("Ladder")) {
+			isLadderStay = false;
+			this.GetComponent<Rigidbody2D> ().gravityScale = 1f;
+		}
+	}
+
+	void FireBullet () {
 		if (isForwardRight) {
-			Instantiate (Bullet, transform.position, transform.rotation);
+			Instantiate (Bullet [BulletID], transform.position, transform.rotation);
 		} else {
-			Instantiate (Bullet, transform.position, new Quaternion(0f,180f,0f,0f));
+			Instantiate (Bullet [BulletID], transform.position, new Quaternion (0f, 180f, 0f, 0f));
 		}
 	}
 }
